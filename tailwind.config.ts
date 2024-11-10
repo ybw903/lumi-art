040b115aa5f2525c92826128790b5d3836bd3fa8
유병우 <conner903@naver.com>
2024-11-10 11:59:10 +0900

    🔍 🍱 seo, font 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        pretendard: ["var(--font-pretendard)"],
+      },
     },
   },
   plugins: [],
