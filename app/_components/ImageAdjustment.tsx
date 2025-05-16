7952b029b5a86b0f1042ac3a9345fa94c6c4ac8e
유병우 <conner903@naver.com>
2025-05-16 15:43:22 +0900

    ♻️ image adjustment 컴포넌트 분리

---
+++ app/_components/ImageAdjustment.tsx
@@ -0,0 +1,279 @@
+import clsx from "clsx";
+import { useState } from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "./Sheet";
+import { Slider } from "./Slider";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerDescription,
+  DrawerHeader,
+  DrawerTitle,
+} from "./Drawer";
+import { useViewSize } from "../_hooks/useViewSize";
+import { useCanvasRendererContext } from "../_contexts/CanvasRendererContext";
+
+enum AdjustmentType {
+  BRIGHTNESS = "brightness",
+  EXPOSURE = "exposure",
+  CONTRAST = "contrast",
+  HIGHLIGHTS = "highlights",
+  SHADOWS = "shadows",
+  SATURATION = "saturation",
+}
+
+const ADJUSTMENT_TYPE_RESOURCES = {
+  [AdjustmentType.BRIGHTNESS]: "밝기",
+  [AdjustmentType.EXPOSURE]: "노출",
+  [AdjustmentType.CONTRAST]: "대비",
+  [AdjustmentType.HIGHLIGHTS]: "하이라이트",
+  [AdjustmentType.SHADOWS]: "섀도우",
+  [AdjustmentType.SATURATION]: "채도",
+} as const satisfies { [key in AdjustmentType]: string };
+
+interface ImageAdjustmentProps {
+  open: boolean;
+  onOpenChange: (open: boolean) => void;
+}
+
+export const ImageAdjustment = ({
+  open,
+  onOpenChange,
+}: ImageAdjustmentProps) => {
+  const { isDesktop, isMobile } = useViewSize();
+  const [adjustmentType, setAdjustmentType] = useState(
+    AdjustmentType.BRIGHTNESS
+  );
+
+  const { adjustments, setAdjustments } = useCanvasRendererContext();
+
+  const handleAdjustmentType = (tab: AdjustmentType) => () => {
+    setAdjustmentType(tab);
+  };
+
+  const handleAdjustment = (value: number, type: AdjustmentType) => {
+    setAdjustments({ ...adjustments, [type]: value });
+  };
+
+  if (!open) return;
+
+  return (
+    <>
+      {isDesktop && (
+        <aside className="w-[360px]">
+          <Sheet
+            defaultOpen={true}
+            open={open}
+            onOpenChange={onOpenChange}
+            modal={false}
+          >
+            <SheetContent
+              side={"left"}
+              onPointerDownOutside={(evt) => evt.preventDefault()}
+              onInteractOutside={(evt) => evt.preventDefault()}
+            >
+              <SheetHeader>
+                <SheetTitle>조정</SheetTitle>
+                <SheetDescription className="hidden">
+                  이미지 필터를 적용합니다.
+                </SheetDescription>
+              </SheetHeader>
+
+              <section className="flex flex-col">
+                <div className="w-full py-2 flex gap-2  overflow-auto">
+                  {Object.values(AdjustmentType).map((AdjustmentType) => (
+                    <button
+                      key={`adjustment-tab-${AdjustmentType}`}
+                      className={clsx(
+                        "w-24 flex-shrink-0 text-center p-2 rounded  font-bold text-white bg-violet-400",
+                        adjustmentType === AdjustmentType && "bg-violet-700",
+                        adjustmentType !== AdjustmentType &&
+                          "hover:bg-violet-500"
+                      )}
+                      onClick={handleAdjustmentType(AdjustmentType)}
+                    >
+                      {ADJUSTMENT_TYPE_RESOURCES[AdjustmentType]}
+                    </button>
+                  ))}
+                </div>
+                <div className="w-full flex flex-col py-4">
+                  {adjustmentType === AdjustmentType.BRIGHTNESS && (
+                    <Slider
+                      value={[adjustments.brightness]}
+                      max={1}
+                      min={-1}
+                      step={0.01}
+                      onValueChange={([value]) =>
+                        handleAdjustment(value, AdjustmentType.BRIGHTNESS)
+                      }
+                    />
+                  )}
+                  {adjustmentType === AdjustmentType.EXPOSURE && (
+                    <Slider
+                      value={[adjustments.exposure]}
+                      max={1}
+                      min={-1}
+                      step={0.01}
+                      onValueChange={([value]) =>
+                        handleAdjustment(value, AdjustmentType.EXPOSURE)
+                      }
+                    />
+                  )}
+                  {adjustmentType === AdjustmentType.CONTRAST && (
+                    <Slider
+                      value={[adjustments.contrast]}
+                      max={1}
+                      min={-1}
+                      step={0.01}
+                      onValueChange={([value]) =>
+                        handleAdjustment(value, AdjustmentType.CONTRAST)
+                      }
+                    />
+                  )}
+                  {adjustmentType === AdjustmentType.HIGHLIGHTS && (
+                    <Slider
+                      value={[adjustments.highlights]}
+                      max={0}
+                      min={-1}
+                      step={0.01}
+                      onValueChange={([value]) =>
+                        handleAdjustment(value, AdjustmentType.HIGHLIGHTS)
+                      }
+                    />
+                  )}
+                  {adjustmentType === AdjustmentType.SHADOWS && (
+                    <Slider
+                      value={[adjustments.shadows]}
+                      max={1}
+                      min={0}
+                      step={0.01}
+                      onValueChange={([value]) =>
+                        handleAdjustment(value, AdjustmentType.SHADOWS)
+                      }
+                    />
+                  )}
+                  {adjustmentType === AdjustmentType.SATURATION && (
+                    <Slider
+                      value={[adjustments.saturation]}
+                      max={1}
+                      min={-1}
+                      step={0.01}
+                      onValueChange={([value]) =>
+                        handleAdjustment(value, AdjustmentType.SATURATION)
+                      }
+                    />
+                  )}
+                </div>
+              </section>
+            </SheetContent>
+          </Sheet>
+        </aside>
+      )}
+      {isMobile && (
+        <Drawer open={open} onOpenChange={onOpenChange} modal={false}>
+          <DrawerContent>
+            <div className="mx-auto w-full max-w-sm">
+              <DrawerHeader>
+                <DrawerTitle>조정</DrawerTitle>
+                <DrawerDescription className="hidden">
+                  이미지 필터를 적용합니다.
+                </DrawerDescription>
+              </DrawerHeader>
+              <section className="flex flex-col px-4">
+                <div className="w-full py-2 flex gap-2 overflow-auto">
+                  {Object.values(AdjustmentType).map((AdjustmentType) => (
+                    <button
+                      key={`adjustment-tab-${AdjustmentType}`}
+                      className={clsx(
+                        "w-24 flex-shrink-0 text-center p-2 rounded  font-bold text-white bg-violet-400",
+                        adjustmentType === AdjustmentType && "bg-violet-700",
+                        adjustmentType !== AdjustmentType &&
+                          "hover:bg-violet-500"
+                      )}
+                      onClick={handleAdjustmentType(AdjustmentType)}
+                    >
+                      {ADJUSTMENT_TYPE_RESOURCES[AdjustmentType]}
+                    </button>
+                  ))}
+                </div>
+                <div className="w-full flex flex-col py-4">
+                  {adjustmentType === AdjustmentType.BRIGHTNESS && (
+                    <Slider
+                      value={[adjustments.brightness]}
+                      max={1}
+                      min={-1}
+                      step={0.01}
+                      onValueChange={([value]) =>
+                        handleAdjustment(value, AdjustmentType.BRIGHTNESS)
+                      }
+                    />
+                  )}
+                  {adjustmentType === AdjustmentType.EXPOSURE && (
+                    <Slider
+                      value={[adjustments.exposure]}
+                      max={1}
+                      min={-1}
+                      step={0.01}
+                      onValueChange={([value]) =>
+                        handleAdjustment(value, AdjustmentType.EXPOSURE)
+                      }
+                    />
+                  )}
+                  {adjustmentType === AdjustmentType.CONTRAST && (
+                    <Slider
+                      value={[adjustments.contrast]}
+                      max={1}
+                      min={-1}
+                      step={0.01}
+                      onValueChange={([value]) =>
+                        handleAdjustment(value, AdjustmentType.CONTRAST)
+                      }
+                    />
+                  )}
+                  {adjustmentType === AdjustmentType.HIGHLIGHTS && (
+                    <Slider
+                      value={[adjustments.highlights]}
+                      max={0}
+                      min={-1}
+                      step={0.01}
+                      onValueChange={([value]) =>
+                        handleAdjustment(value, AdjustmentType.HIGHLIGHTS)
+                      }
+                    />
+                  )}
+                  {adjustmentType === AdjustmentType.SHADOWS && (
+                    <Slider
+                      value={[adjustments.shadows]}
+                      max={1}
+                      min={0}
+                      step={0.01}
+                      onValueChange={([value]) =>
+                        handleAdjustment(value, AdjustmentType.SHADOWS)
+                      }
+                    />
+                  )}
+                  {adjustmentType === AdjustmentType.SATURATION && (
+                    <Slider
+                      value={[adjustments.saturation]}
+                      max={1}
+                      min={-1}
+                      step={0.01}
+                      onValueChange={([value]) =>
+                        handleAdjustment(value, AdjustmentType.SATURATION)
+                      }
+                    />
+                  )}
+                </div>
+              </section>
+            </div>
+          </DrawerContent>
+        </Drawer>
+      )}
+    </>
+  );
+};
--- app/design/page.tsx
+++ app/design/page.tsx
@@ -4,43 +4,12 @@ import clsx from "clsx";
 import { useEffect, useRef, useState } from "react";
 import { ImageIcon } from "lucide-react";
 import { v4 as uuidv4 } from "uuid";
-import { Slider } from "../_components/Slider";
+
 import { SUPPORTED_MIME_TYPES } from "../_constants/file";
+import { ImageAdjustment } from "../_components/ImageAdjustment";
 import { useCanvasRendererContext } from "../_contexts/CanvasRendererContext";
 import { usePreviewImg } from "../_hooks/usePreviewImage";
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-} from "../_components/Sheet";
 import { useViewSize } from "../_hooks/useViewSize";
-import {
-  Drawer,
-  DrawerContent,
-  DrawerDescription,
-  DrawerHeader,
-  DrawerTitle,
-} from "../_components/Drawer";
-
-enum AdjustmentType {
-  BRIGHTNESS = "brightness",
-  EXPOSURE = "exposure",
-  CONTRAST = "contrast",
-  HIGHLIGHTS = "highlights",
-  SHADOWS = "shadows",
-  SATURATION = "saturation",
-}
-
-const ADJUSTMENT_TYPE_RESOURCES = {
-  [AdjustmentType.BRIGHTNESS]: "밝기",
-  [AdjustmentType.EXPOSURE]: "노출",
-  [AdjustmentType.CONTRAST]: "대비",
-  [AdjustmentType.HIGHLIGHTS]: "하이라이트",
-  [AdjustmentType.SHADOWS]: "섀도우",
-  [AdjustmentType.SATURATION]: "채도",
-} as const satisfies { [key in AdjustmentType]: string };
 
 export default function DesignPage() {
   const imageRef = useRef<HTMLImageElement | null>(null);
@@ -48,18 +17,11 @@ export default function DesignPage() {
   const canvasContainerRef = useRef<HTMLDivElement | null>(null);
   const imageInputRef = useRef<HTMLInputElement | null>(null);
 
-  const {
-    adjustments,
-    render,
-    createCanvasRenderer,
-    setImage,
-    setAdjustments,
-  } = useCanvasRendererContext();
+  const { render, createCanvasRenderer, setImage } = useCanvasRendererContext();
 
-  const { isDesktop, isMobile } = useViewSize();
+  const { isDesktop } = useViewSize();
 
-  const [openedAdjustmentPanel, setOpenedAdjustmentPanel] = useState(false);
-  const [tab, setTab] = useState(AdjustmentType.BRIGHTNESS);
+  const [openedImageAdjustment, setOpenedImageAdjustment] = useState(false);
 
   const [imageFile, setImageFile] = useState<File | null>(null);
   const imageDataUrl = usePreviewImg(imageFile);
@@ -72,7 +34,7 @@ export default function DesignPage() {
   const handleUploadImage = (evt: React.ChangeEvent<HTMLInputElement>) => {
     const imgFile = evt.target.files?.[0];
     if (!imgFile) return;
-    setOpenedAdjustmentPanel(true);
+    setOpenedImageAdjustment(true);
     setImageFile(imgFile);
   };
 
@@ -94,14 +56,6 @@ export default function DesignPage() {
     setImage(imageRef.current);
   };
 
-  const handleTab = (tab: AdjustmentType) => () => {
-    setTab(tab);
-  };
-
-  const handleAdjustment = (value: number, type: AdjustmentType) => {
-    setAdjustments({ ...adjustments, [type]: value });
-  };
-
   useEffect(() => {
     if (!imageDataUrl) return;
     if (!canvasRef.current || !canvasContainerRef.current) return;
@@ -119,255 +73,19 @@ export default function DesignPage() {
 
   return (
     <div className="w-full flex justify-center items-center min-h-screen">
-      {isDesktop && openedAdjustmentPanel && (
-        <aside className="w-[360px]">
-          <Sheet
-            defaultOpen={true}
-            open={openedAdjustmentPanel}
-            onOpenChange={setOpenedAdjustmentPanel}
-            modal={false}
-          >
-            <SheetContent
-              side={"left"}
-              onPointerDownOutside={(evt) => evt.preventDefault()}
-              onInteractOutside={(evt) => evt.preventDefault()}
-            >
-              <SheetHeader>
-                <SheetTitle>조정</SheetTitle>
-                <SheetDescription className="hidden">
-                  이미지 필터를 적용합니다.
-                </SheetDescription>
-              </SheetHeader>
-
-              {imageDataUrl && (
-                <section className="flex flex-col">
-                  <div className="w-full py-2 flex gap-2  overflow-auto">
-                    {Object.values(AdjustmentType).map((AdjustmentType) => (
-                      <button
-                        key={`adjustment-tab-${AdjustmentType}`}
-                        className={clsx(
-                          "w-24 flex-shrink-0 text-center p-2 rounded  font-bold text-white bg-violet-400",
-                          tab === AdjustmentType && "bg-violet-700",
-                          tab !== AdjustmentType && "hover:bg-violet-500"
-                        )}
-                        onClick={handleTab(AdjustmentType)}
-                      >
-                        {ADJUSTMENT_TYPE_RESOURCES[AdjustmentType]}
-                      </button>
-                    ))}
-                  </div>
-                  <div className="w-full flex flex-col py-4">
-                    {tab === AdjustmentType.BRIGHTNESS && (
-                      <>
-                        <Slider
-                          value={[adjustments.brightness]}
-                          max={1}
-                          min={-1}
-                          step={0.01}
-                          onValueChange={([value]) =>
-                            handleAdjustment(value, AdjustmentType.BRIGHTNESS)
-                          }
-                        />
-                        {/* <div>
-                    <span className="text-red-600">RED</span>
-                    <Slider
-                      value={[rBrightness]}
-                      max={100}
-                      step={1}
-                      onValueChange={([r]) => setRBrightness(r)}
-                    />
-                  </div>
-                  */}
-                      </>
-                    )}
-                    {tab === AdjustmentType.EXPOSURE && (
-                      <Slider
-                        value={[adjustments.exposure]}
-                        max={1}
-                        min={-1}
-                        step={0.01}
-                        onValueChange={([value]) =>
-                          handleAdjustment(value, AdjustmentType.EXPOSURE)
-                        }
-                      />
-                    )}
-                    {tab === AdjustmentType.CONTRAST && (
-                      <Slider
-                        value={[adjustments.contrast]}
-                        max={1}
-                        min={-1}
-                        step={0.01}
-                        onValueChange={([value]) =>
-                          handleAdjustment(value, AdjustmentType.CONTRAST)
-                        }
-                      />
-                    )}
-                    {tab === AdjustmentType.HIGHLIGHTS && (
-                      <Slider
-                        value={[adjustments.highlights]}
-                        max={0}
-                        min={-1}
-                        step={0.01}
-                        onValueChange={([value]) =>
-                          handleAdjustment(value, AdjustmentType.HIGHLIGHTS)
-                        }
-                      />
-                    )}
-                    {tab === AdjustmentType.SHADOWS && (
-                      <Slider
-                        value={[adjustments.shadows]}
-                        max={1}
-                        min={0}
-                        step={0.01}
-                        onValueChange={([value]) =>
-                          handleAdjustment(value, AdjustmentType.SHADOWS)
-                        }
-                      />
-                    )}
-                    {tab === AdjustmentType.SATURATION && (
-                      <Slider
-                        value={[adjustments.saturation]}
-                        max={1}
-                        min={-1}
-                        step={0.01}
-                        onValueChange={([value]) =>
-                          handleAdjustment(value, AdjustmentType.SATURATION)
-                        }
-                      />
-                    )}
-                  </div>
-                </section>
-              )}
-            </SheetContent>
-          </Sheet>
-        </aside>
-      )}
-      {isMobile && openedAdjustmentPanel && (
-        <Drawer
-          open={openedAdjustmentPanel}
-          onOpenChange={setOpenedAdjustmentPanel}
-          modal={false}
-        >
-          <DrawerContent>
-            <div className="mx-auto w-full max-w-sm">
-              <DrawerHeader>
-                <DrawerTitle>조정</DrawerTitle>
-                <DrawerDescription className="hidden">
-                  이미지 필터를 적용합니다.
-                </DrawerDescription>
-              </DrawerHeader>
-              {imageDataUrl && (
-                <section className="flex flex-col px-4">
-                  <div className="w-full py-2 flex gap-2 overflow-auto">
-                    {Object.values(AdjustmentType).map((AdjustmentType) => (
-                      <button
-                        key={`adjustment-tab-${AdjustmentType}`}
-                        className={clsx(
-                          "w-24 flex-shrink-0 text-center p-2 rounded  font-bold text-white bg-violet-400",
-                          tab === AdjustmentType && "bg-violet-700",
-                          tab !== AdjustmentType && "hover:bg-violet-500"
-                        )}
-                        onClick={handleTab(AdjustmentType)}
-                      >
-                        {ADJUSTMENT_TYPE_RESOURCES[AdjustmentType]}
-                      </button>
-                    ))}
-                  </div>
-                  <div className="w-full flex flex-col py-4">
-                    {tab === AdjustmentType.BRIGHTNESS && (
-                      <>
-                        <Slider
-                          value={[adjustments.brightness]}
-                          max={1}
-                          min={-1}
-                          step={0.01}
-                          onValueChange={([value]) =>
-                            handleAdjustment(value, AdjustmentType.BRIGHTNESS)
-                          }
-                        />
-                        {/* <div>
-                    <span className="text-red-600">RED</span>
-                    <Slider
-                      value={[rBrightness]}
-                      max={100}
-                      step={1}
-                      onValueChange={([r]) => setRBrightness(r)}
-                    />
-                  </div>
-                  */}
-                      </>
-                    )}
-                    {tab === AdjustmentType.EXPOSURE && (
-                      <Slider
-                        value={[adjustments.exposure]}
-                        max={1}
-                        min={-1}
-                        step={0.01}
-                        onValueChange={([value]) =>
-                          handleAdjustment(value, AdjustmentType.EXPOSURE)
-                        }
-                      />
-                    )}
-                    {tab === AdjustmentType.CONTRAST && (
-                      <Slider
-                        value={[adjustments.contrast]}
-                        max={1}
-                        min={-1}
-                        step={0.01}
-                        onValueChange={([value]) =>
-                          handleAdjustment(value, AdjustmentType.CONTRAST)
-                        }
-                      />
-                    )}
-                    {tab === AdjustmentType.HIGHLIGHTS && (
-                      <Slider
-                        value={[adjustments.highlights]}
-                        max={0}
-                        min={-1}
-                        step={0.01}
-                        onValueChange={([value]) =>
-                          handleAdjustment(value, AdjustmentType.HIGHLIGHTS)
-                        }
-                      />
-                    )}
-                    {tab === AdjustmentType.SHADOWS && (
-                      <Slider
-                        value={[adjustments.shadows]}
-                        max={1}
-                        min={0}
-                        step={0.01}
-                        onValueChange={([value]) =>
-                          handleAdjustment(value, AdjustmentType.SHADOWS)
-                        }
-                      />
-                    )}
-                    {tab === AdjustmentType.SATURATION && (
-                      <Slider
-                        value={[adjustments.saturation]}
-                        max={1}
-                        min={-1}
-                        step={0.01}
-                        onValueChange={([value]) =>
-                          handleAdjustment(value, AdjustmentType.SATURATION)
-                        }
-                      />
-                    )}
-                  </div>
-                </section>
-              )}
-            </div>
-          </DrawerContent>
-        </Drawer>
-      )}
+      <ImageAdjustment
+        open={openedImageAdjustment}
+        onOpenChange={setOpenedImageAdjustment}
+      />
 
       <main
         className={clsx(
           "mt-8 flex flex-col items-center",
-          isDesktop && openedAdjustmentPanel && "w-[100%-360px]"
+          isDesktop && openedImageAdjustment && "w-[100%-360px]"
         )}
       >
         <section className="my-8 flex gap-4 relative">
-          {imageDataUrl && !openedAdjustmentPanel && (
+          {imageDataUrl && !openedImageAdjustment && (
             <div
               className={clsx(
                 "absolute top-[-48px] py-2 px-2.5 rounded-xl bg-white shadow-md"
@@ -375,7 +93,7 @@ export default function DesignPage() {
             >
               <button
                 className="flex items-center cursor-pointer"
-                onClick={() => setOpenedAdjustmentPanel(true)}
+                onClick={() => setOpenedImageAdjustment(true)}
               >
                 <ImageIcon size={14} />
                 <span className="ml-1 text-xs font-bold">편집</span>
